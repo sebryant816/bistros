cd9e1c5eff55f9ff88874370acd966b82b13ff27
Stephen Bryant <sebryant816@yahoo.com>
2019-05-28 10:58:47 -0400

    Add files via upload

---
+++ bistros/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<style>
+  
+  
+
+</style>    
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<title>BISTRO</title>
+
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+<title>Bistros</title>
+
+      <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!---Navigation Bar-->
+
+   <nav class="navbar navbar-light" style="background-image: url(images/navback.jpg); background-repeat: no-repeat; background-size: 100%;">
+     <div class="container-fluid" style="border-bottom: 3px solid #003300; border-top: 3px solid #003300;">
+      <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#" style="padding-bottom:70px;margin-top:30px; font-family: 'Pacifico', cursive;font-size:50px;color:#003300;"> Bistros</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right"style="margin-top:30px;">
+          <li><a href="#" style="color:#003300;font-size:20px;">Reservations</a></li>
+          <li><a href="#" style="color:#003300;font-size:20px;">Jobs</a></li> 
+          <li><a href="#" style="color:#003300;font-size:20px;">Contact Us</a></li> 
+        </ul>
+      </div>
+     </div>
+   </nav>
+
+   <!--Defining Jumbotron-->
+
+  <div class="container text-center">
+       <div class="row">
+           <div class="col-md-12" style="border-bottom: 3px solid #003300;">
+               <div class="jumbotron" style="background-color: white;">
+                  <h1 style="color:#006600;"> Voted Best New Restaurant in Jacksonville</h1>
+                  <br><br>
+                  <p style="font-size:18px; color:black;">Lorem ipsum dolor sit amet, est iuvaret appetere ea, et vel summ
+                  o malorum tincidunt, te nam modo Sea clita <br>feugiat cu, mei ea gubergren laboramus. Vel an ornatus fas<br>
+                  <br><br>
+                  <button type="button" class="btn btn-success btn-lg">Order Online</button></p> 
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+    <br><br>
+    <br><br>
+
+
+
+
+<!--Defining columns-->
+
+<div class="container text-center" style="border-bottom: 3px solid #003300; padding-bottom: 70px;">
+    <div class="row">
+      <h2 style="font-size:40px">Our Menus</h2>
+        <div class="col-md-4">
+            <br><br>
+            <img src="images/breakfast.jpg" style="width:100%">
+            <br><br>
+            <p style="font-size: 20px">Breakfast</p>
+            </div>
+
+        <div class="col-md-4">
+            <br><br>
+            <img src="images/lunch.jpg" style="width:100%">
+            <br><br>
+             <p style="font-size: 20px">Lunch</p>
+            </div>
+
+        <div class="col-md-4">
+            <br><br>
+            <img src="images/dinner.jpg" style="width:365px; height: 240px;">
+            <br><br>
+            <p style="font-size: 20px">Dinner</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="container" style="padding-top:30px;padding-bottom:30px; background-color:#cc9900;border-right:2px solid #003300; border-left:2px solid #003300; border-bottoms:3px solid #003300;">
+    <div class="row" style="margin-left:50px">
+        <div class="col-lg-6">
+            <h3>Our Food</h3>
+            <br>
+            <h5>Lorem ipsum dolor aliquip.Lorem ipsum dolor aliquip.Lorem ipuip.Lorem ipsum dolor aliquip.Lorem isum dolor aliquip.Lorem ipsum dolor aliquip.Lorem ipsum dolor aliquip.</h5>
+
+            <h5>Lorem ipsum dolor aliquip.Lorem ipsum doluip.Lorem ipsum dolor aliquip.Lorem ior aliquip.Lorem ipsum dolor aliquip.Lorem ipsum dolor aliquip.Lorem ipsum dolor aliquip.</h5>
+        </div>
+    
+        <div class="col-lg-6">
+            <h3>Reviews</h3>
+            <br>
+
+<!-----<img src="images/review1.jpg" class="img-thumbnail" alt="reviewer 1" width="50" height="80">--->
+            <h5> <img src="images/review1.jpg" class="img-thumbnail" alt="reviewer 1" width="50" height="80" style="background-color:black; border-style:none;">&nbsp;&nbsp;Lorem ipsum dolor aliquip.Lorem ipsum dolaor</h5>
+            <h5><img src="images/review2.jpg" class="img-thumbnail" alt="reviewer 1" width="50" height="80" style="background-color:black; border-style:none;">&nbsp;&nbsp;Lorem ipsum dolor aliquip.Lorem ipsum dolui</h5>
+         </div >
+    </div>
+</div>
+
+<!---Footer here--->
+<br><br>
+<nav class="navbar navbar-light" style="background-image: url(images/navback.jpg); background-repeat: no-repeat; background-size: 100%;">
+  <div class="container-fluid" style="border-top: 3px solid #003300; border-bottom: 3px solid #003300;">
+    <div class="navbar-footer">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#" style="font-family: 'Pacifico', cursive;font-size:50px;color:#003300;"> Bistros</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right"style="margin-top:10px;">
+        <li><a href="#" style="color:#003300;font-size:20px;">Reservations</a></li>
+        <li><a href="#" style="color:#003300;font-size:20px;">Jobs</a></li> 
+        <li><a href="#" style="color:#003300;font-size:20px;">Contact Us</a></li> 
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</body>
+
+</html>
+
